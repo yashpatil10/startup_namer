032860a89028924888454ad81a18643a4078c4b9
yashwant patil <iamyashpatil10@gmail.com>
2022-08-17 19:52:59 +0530

    new addition on 17 aug of final update in startup namer

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,21 @@ void main() {
 //or// void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Welcome to flutter',
-      home: RandomWords(),
+    
+    return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
+
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      )),
+      home: const RandomWords(),
     );
   }
 }
@@ -49,20 +59,25 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[]; //it is list of data type wordpairs
-  
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{}; //new_stuff
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Startup Name Generator'),backgroundColor: Color.fromARGB(255, 187, 201, 172) ),
-      body: _buildSuggestions(),
-    );
-  }
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions ',
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-
-      /*
+          /*
       7.1.
       ListView gives list but builder ables the edit in it.
 
@@ -78,35 +93,75 @@ class _RandomWordsState extends State<RandomWords> {
       example :- 16 then index is 8;
 
       */
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); //this is infinity 
-        }
-        // if the suggestion generated are only 10 but index is 12 , 
-        // so it will add suggestion in that list by using, 
-        // the generateWordPairs() function
-        // this generates infinity loop , but your device will not crash , 
-        // as it will takes years of scrolling.
-        return _buildRow(_suggestions[index]);
-      },
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions
+                  .addAll(generateWordPairs().take(10)); //this is infinity
+            }
+            // if the suggestion generated are only 10 but index is 12 ,
+            // so it will add suggestion in that list by using,
+            // the generateWordPairs() function
+            // this generates infinity loop , but your device will not crash ,
+            // as it will takes years of scrolling.
+            final alreadySaved = _saved.contains(_suggestions[index]);
+            return ListTile(
+                title: Text(
+                  _suggestions[index].asPascalCase,
+                  style: _biggerFont,
+                ),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? 'Remove from saved ' : 'Save',
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                });
+          },
+        ));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 }
-
-Widget _buildRow(WordPair pair){
-  const _biggerFont = TextStyle(fontSize: 18);
-  return ListTile(
-          title: Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-            
-          ),
-        );
-}
-
 
 /*
 1.
@@ -135,4 +190,47 @@ here it is _RandomWordsState.
 Add a _suggestions list for saving suggested word pairings.  
 Add a _biggerFont variable for making the font size larger.
 
+8.
+workshop (part2)
+ --> creating a _saved var or set for storing favorite names, in 
+_RandomWordsState.
+
+--> trailing : is used for aligning any thing to extreme right, 
+here icon is placed 
+
+--> in Icon we are using alreadySaved as a bool 
+and 
+if it is saved
+  alreadySaved ? Icons.favorite : Icons.favorite_border, // if saved then favorite icon else only border
+  color: alreadySaved ? Colors.red : null,                //if saved then color red or null
+  semanticLabel: alreadySaved ? 'Remove from saved ' : 'Save', 
+
+9.
+adding a new page called route in flutter, which displays favorites
+the Navigator manages a stack containing the app's routes. Pushing a route onto the Navigator's stack
+updates the display to that route. Popping a route from
+the Navigator's stack returns the display 
+
+
+10.
+need to add list view in title, so "action" is added in the appBar section
+with IconButton
+
+11.
+ build a route and push it to the Navigator's stack,
+ this action displays new screen, call " Navigator.push " 
+
+ 12. what is (build context) or (context)
+ in build method 
+ widget does not keep track of where it is placed on screen , 
+ so in stateless and statefull widget they are made form class widget
+ in this base class we have "Element" which keeps track of the widget
+ so here the Element is type of build context
+
+ 13.
+ MaterialPageRoute and its builder. For now, add the code that 
+ generates the ListTile rows. The divideTiles() method of ListTile 
+ adds horizontal spacing between each ListTile. The divided variable 
+ holds the final rows converted to a list by the convenience function, 
+ toList()
 */
